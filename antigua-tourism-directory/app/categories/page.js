e2b25e6a2ada489f8cafc733702f3e161930a485
Jeff Stapley <jeff.stapley@live.ca>
2026-03-08 08:00:20 -0400

    update search page

--- antigua-tourism-directory/app/categories/page.js
+++ antigua-tourism-directory/app/categories/page.js
@@ -4,8 +4,33 @@ import CategoriesPageClient from './CategoriesPageClient'
 export const revalidate = 3600
 
 export const metadata = {
-  title: 'Browse All Categories - Antigua Search',
-  description: 'Explore all business categories in Antigua & Barbuda - restaurants, hotels, tours, activities, and more',
+  title: 'Browse All Categories | AntiguaSearch.com',
+  description: 'Explore all business categories in Antigua & Barbuda - restaurants, hotels, tours, activities, shopping, and more.',
+  alternates: {
+    canonical: 'https://www.antiguasearch.com/categories',
+  },
+  openGraph: {
+    title: 'Browse All Categories | AntiguaSearch.com',
+    description: 'Explore all business categories in Antigua & Barbuda - restaurants, hotels, tours, activities, shopping, and more.',
+    url: 'https://www.antiguasearch.com/categories',
+    siteName: 'AntiguaSearch.com',
+    locale: 'en_US',
+    type: 'website',
+    images: [
+      {
+        url: 'https://www.antiguasearch.com/og-image.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'AntiguaSearch.com - Business Directory',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Browse All Categories | AntiguaSearch.com',
+    description: 'Explore all business categories in Antigua & Barbuda.',
+    images: ['https://www.antiguasearch.com/og-image.jpg'],
+  },
 }
 
 async function getCategories() {
@@ -19,6 +44,5 @@ async function getCategories() {
 
 export default async function CategoriesPage() {
   const categories = await getCategories()
-
   return <CategoriesPageClient categories={categories} />
 }
